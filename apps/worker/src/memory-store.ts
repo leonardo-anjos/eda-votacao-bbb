f07302e618f4b308b7309036704398e76f11340f
Leonardo Anjos <leodrums11@gmail.com>
2025-04-27 23:47:04 -0300

    wip: remove memory storage

--- apps/worker/src/memory-store.ts
+++
@@ -1,9 +0,0 @@
-const votes: Record<string, number> = {};
-
-export function saveVote(participant: string) {
-  votes[participant] = (votes[participant] || 0) + 1;
-}
-
-export function getVotes() {
-  return votes;
-}
